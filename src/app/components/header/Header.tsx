d7bc380491f8ea8b99b4778513c5639d4bce1e72
John <jhonfe649@gmail.com>
2024-09-20 23:33:25 -0500

    fix(header) replace a for link component

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -25,14 +25,14 @@ function Header() {
         aria-label="Global"
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
       >
-        <a href="#" className="-m-1.5 p-1.5">
-          <span className="sr-only">Your Company</span>
+        <Link href="#" className="-m-1.5 p-1.5">
+          <span className="sr-only">Mr gomma</span>
           <img
             alt=""
             src="images/logo-mrgoma.webp"
             className="h-8 w-auto"
           />
-        </a>
+        </Link>
         <div className="flex lg:hidden">
           <button
             type="button"
@@ -45,13 +45,13 @@ function Header() {
         </div>
         <div className="hidden lg:flex lg:gap-x-12 ">
           {navigation.map((item) => (
-            <a
+            <Link
               key={item.name}
               href={item.href}
               className="text-sm font-semibold leading-6 text-white"
             >
               {item.name}
-            </a>
+            </Link>
           ))}
         </div>
       </nav>
@@ -63,14 +63,14 @@ function Header() {
         <div className="fixed inset-0 z-10" />
         <DialogPanel className="fixed inset-y-0 left-0 z-50 w-80 overflow-y-auto bg-white px-6 py-6 sm:w-sm sm:ring-1 sm:ring-gray-900/10 shadow-xl">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5 bg-black rounded">
+            <Link href="#" className="-m-1.5 p-1.5 bg-black rounded">
               <span className="sr-only">Mr gomma</span>
               <img
                 alt="mr gomma logo"
                 src="images/logo-mrgoma.webp"
                 className="h-8 w-auto"
               />
-            </a>
+            </Link>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
@@ -84,13 +84,13 @@ function Header() {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
             </div>
