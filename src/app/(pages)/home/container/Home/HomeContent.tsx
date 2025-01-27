f28bcb08f50e17e4378d55ebe6f2514200bc48df
Alejandro Palacios Arévalo <alejandro.palacios88@gmail.com>
2025-01-27 18:54:38 -0500

    Update HomeContent.tsx

--- src/app/(pages)/home/container/Home/HomeContent.tsx
+++ src/app/(pages)/home/container/Home/HomeContent.tsx
@@ -1,1 +1,39 @@
+'use client';
+
+import { FC } from 'react';
+
+import { SearchTabs, Title } from '@/app/ui/components';
+import { MobileTopFilters, TopFilter } from '@/app/ui/sections';
+
+const HomeContent: FC = () => {
+  return (
+    <main className="bg-white">
+      <div>
+        <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="sm:mb-16 mt-16">
+            <Title />
+          </div>
+          <section aria-labelledby="products-heading" className="pb-24">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
+              <div className="lg:col-span-3">
+                <div className="hidden sm:block sticky top-0 z-40 bg-gray-100 p-2 rounded-lg shadow-lg">
+                  <SearchTabs />
+                </div>
+                <div className="sticky block sm:hidden top-0 z-40 bg-white rounded-b-lg shadow-md my-4">
+                  <MobileTopFilters>
+                    <div className="p-4">
+                      <TopFilter />
+                    </div>
+                  </MobileTopFilters>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+      </div>
+    </main>
+  );
+};
+
+export default HomeContent;
 
