d17122aa898236d39462c067f9f7474490af3b38
Alejandro Palacios Arévalo <alejandro.palacios88@gmail.com>
2025-01-27 18:52:53 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,15 +14,6 @@ const nextConfig = {
       },
     ],
   },
-  redirects: async () => {
-    return [
-      {
-        source: '/',
-        destination: '/catalog',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
