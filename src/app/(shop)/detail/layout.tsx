8dbc09a8b3b8d6b16f6509061dbb4a00a9c22670
Alejandro Palacios <alejandro.palacios88@gmail.com>
2026-03-27 21:02:36 -0500

    feat: remove metadata from layout components to streamline SEO implementation

--- src/app/(shop)/detail/layout.tsx
+++ src/app/(shop)/detail/layout.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
 
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'MrGoma Tires',
-  description: 'MrGoma Tires is a tire shop in Miami, FL',
-};
-
 export default function DetailLayout({
   children,
 }: Readonly<{
-
-
   children: React.ReactNode;
 }>) {
   return <>{children}</>;
